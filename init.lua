13ac39b8f8f541119ed860a756552d727dacf1ef
ktunprasert <ktunprasert@outlook.com>
2022-06-26 18:47:52 +0100

    nvim: lua files init

---
+++ init.lua
@@ -0,0 +1,5 @@
+-- Global neovim dotfiles for configuring exactly your Neovim
+--
+--
+-- User Options
+require "user.options"
---
+++ lua/user/options.lua
@@ -0,0 +1,35 @@
+-- :help options
+vim.opt.backup = false
+vim.opt.clipboard = "unnamedplus"
+vim.opt.cmdheight = 2
+vim.opt.completeopt = { "menuone", "noselect" } -- for cmp
+vim.opt.conceallevel = 0
+vim.opt.fileencoding = "utf-8"
+vim.opt.hlsearch = true
+vim.opt.mouse = "a"
+vim.opt.pumheight= 10
+vim.opt.showmode = false
+vim.opt.showtabline = 2
+vim.opt.smartcase = true
+vim.opt.smartindent = true
+vim.opt.autoindent = true
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.swapfile = false
+-- vim.opt.termguicolors = true
+vim.opt.timeoutlen = 1000
+vim.opt.undofile = true
+vim.opt.updatetime = 300 -- faster completion
+vim.opt.hidden = true
+vim.opt.writebackup = false
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.number = true
+vim.opt.numberwidth = 4
+vim.opt.signcolumn = "yes"
+vim.opt.wrap = false
+vim.opt.scrolloff = 8
+vim.opt.sidescrolloff = 8
+
+vim.opt.shortmess:append "c"
