9879a9bf2aa944ecae4ede1a8ac467b3413bf87d
ktunprasert <ktunprasert@outlook.com>
2022-06-28 22:21:46 +0100

    more hoppity keybind

--- lua/user/plugins/hop.lua
+++ lua/user/plugins/hop.lua
@@ -7,15 +7,18 @@ local hop_options = { silent = true }
 
 keymap("n", "<Tab>s", ":HopChar1<CR>", hop_options)
 keymap("n", "<Tab>d", ":HopChar2<CR>", hop_options)
+keymap("n", "<Tab><Space>", ":HopChar2<CR>", hop_options)
 
 keymap("n", "<Tab>w", ":HopWordCurrentLine<CR>", hop_options)
 keymap("n", "<Tab>W", ":HopWord<CR>", hop_options)
+keymap("n", "<Tab><Tab>", ":HopWord<CR>", hop_options)
 
 keymap("n", "<Tab>h", ":HopAnywhereCurrentLineBC<CR>", hop_options)
 keymap("n", "<Tab>l", ":HopAnywhereCurrentLineAC<CR>", hop_options)
 
 keymap("n", "<Tab>k", ":HopLineBC<CR>", hop_options)
 keymap("n", "<Tab>j", ":HopLineAC<CR>", hop_options)
+keymap("n", "<Tab>0", ":HopLine<CR>", hop_options)
 
 keymap("n", "<Tab>g", ":HopAnywhere<CR>", hop_options)
 keymap("n", "<Tab>G", ":HopAnywhereMW<CR>", hop_options)
