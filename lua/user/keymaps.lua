94142d202f93d696cc7a9c1206582fe53acb2be2
ktunprasert <ktunprasert@outlook.com>
2022-06-29 01:00:54 +0100

    insert mode suggestion trigger | cmd mode navigate with jk

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -50,7 +50,7 @@ keymap("n", "<A-5>", "5gt")
 keymap("n", "<A-6>", "6gt")
 
 -- Toggle File Explorer
-keymap("n", "<Leader>e", ":Lex 30<CR>")
+keymap("n", "<C-e>", ":Lex 30<CR>")
 
 -- Navigate buffers
 keymap("n", "<S-l>", ":bnext<CR>")
@@ -86,4 +86,17 @@ keymap("n", "<C-Right>", ":vert resize +2<CR>")
 keymap("n", "<Leader><Leader>ec", "<cmd>exe 'edit' stdpath('config')<CR>")
 keymap("n", "<Leader><Leader>ed", "<cmd>exe 'cd' stdpath('config')<CR>")
 
+-- Clear all the search highlight from the screen
 keymap("n", "<F5>", "<cmd>noh<CR>")
+
+-- Change the suggestion scrolling - toggling suggestion with Ctrl + Space in Insert mode
+-- Insert --
+vim.keymap.set("i", "<C-Space>", function() return vim.fn.pumvisible() == 0 and '<C-N>' or '<C-Space>' end, {expr = true})
+vim.keymap.set("i", "<Tab>", function() return vim.fn.pumvisible() == 1 and '<C-N>' or '<Tab>' end, {expr = true})
+vim.keymap.set("i", "<S-Tab>", function() return vim.fn.pumvisible() == 1 and '<C-P>' or '<S-Tab>' end, {expr = true})
+vim.keymap.set("i", "<C-j>", function() return vim.fn.pumvisible() == 1 and '<C-N>' or '<C-j>' end, {expr = true})
+vim.keymap.set("i", "<C-k>", function() return vim.fn.pumvisible() == 1 and '<C-P>' or '<C-k>' end, {expr = true})
+
+-- -- Command --
+vim.keymap.set("c", "<C-j>", 'pumvisible() ? "\\<C-n>" : "\\<C-j>"', { expr = true, noremap = true})
+vim.keymap.set("c", "<C-k>", 'pumvisible() ? "\\<C-p>" : "\\<C-k>"', { expr = true, noremap = true})
