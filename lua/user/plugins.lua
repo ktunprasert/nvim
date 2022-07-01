9cd34e1d3cd06163edfe3a5c934ea938c590e751
ktunprasert <ktunprasert@outlook.com>
2022-07-01 15:20:20 +0100

    added indent line plugin

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -105,6 +105,8 @@ return packer.startup(function(use)
         tag = 'v1.*',
         config = function() require("user.plugins.toggleterm") end
     }
+    use "lukas-reineke/indent-blankline.nvim"
+
     -- Automatically set up your configuration after cloning packer.nvim
     -- Put this at the end after all plugins
     if PACKER_BOOTSTRAP then
