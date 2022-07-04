013010146a981188673019986e63012c4bc5a4e8
ktunprasert <ktunprasert@outlook.com>
2022-07-04 21:43:55 +0100

    whichkey riced

--- lua/user/plugins/whichkey.lua
+++ lua/user/plugins/whichkey.lua
@@ -23,7 +23,7 @@ whichkey.setup {
     },
     icons = {
         breadcrumb = "»", -- symbol used in the command line area that shows your active key combo
-        separator = "➜", -- symbol used between a key and it's label
+        separator = "-", -- symbol used between a key and it's label
         group = "+", -- symbol prepended to a group
     },
     popup_mappings = {
@@ -31,17 +31,17 @@ whichkey.setup {
         scroll_up = "<c-u>", -- binding to scroll up inside the popup
     },
     window = {
-        border = "single", -- none, single, double, shadow
+        border = "none", -- none, single, double, shadow
         position = "bottom", -- bottom, top
-        margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
-        padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
+        margin = { 0, 0, 0, 0 }, -- extra window margin [top, right, bottom, left]
+        padding = { 1, 1, 1, 1 }, -- extra window padding [top, right, bottom, left]
         winblend = 0,
     },
     layout = {
         height = { min = 4, max = 25 }, -- min and max height of the columns
         width = { min = 20, max = 50 }, -- min and max width of the columns
         spacing = 3, -- spacing between columns
-        align = "left", -- align columns left, center or right
+        align = "center", -- align columns left, center or right
     },
     hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "call", "lua", "^:", "^ " }, -- hide mapping boilerplate
     ignore_missing = false, -- enable this to hide mappings for which you didn't specify a label
