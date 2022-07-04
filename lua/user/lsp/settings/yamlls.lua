b2a3126e40e2076f5f0973ba1432c411b10db236
ktunprasert <ktunprasert@outlook.com>
2022-07-04 10:25:22 +0100

    add gitlab-ci to yamlls

--- lua/user/lsp/settings/yamlls.lua
+++ lua/user/lsp/settings/yamlls.lua
@@ -1,6 +1,7 @@
 -- Find more schemas here: https://www.schemastore.org/json/
 local schemas = {
     ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "docker-compose.*",
+    ["https://gitlab.com/gitlab-org/gitlab/-/raw/master/app/assets/javascripts/editor/schema/ci.json"] = ".gitlab-ci.yml",
 }
 
 return {
