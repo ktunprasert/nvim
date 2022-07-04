13307ebc757db54fe02413270a00738ce9ed0bc0
ktunprasert <ktunprasert@outlook.com>
2022-07-04 23:23:46 +0100

    more hop keybinds

--- lua/user/plugins/hop.lua
+++ lua/user/plugins/hop.lua
@@ -5,27 +5,49 @@ end
 
 local keymap = require 'lib.utils'.keymap
 
-hop.setup()
+hop.setup {
+    case_insensitive = false,
+}
 
 local hop_options = { silent = true }
+local modes = { "n", "v" }
 
-keymap("n", "<Tab>s", ":HopChar1<CR>", hop_options)
-keymap("n", "<Tab>d", ":HopChar2<CR>", hop_options)
-keymap("n", "<Tab><Space>", ":HopChar2<CR>", hop_options)
+keymap(modes, "<Tab>s", "<cmd>HopChar2<CR>", hop_options)
+keymap(modes, "<Tab>d", "<cmd>HopChar2<CR>", hop_options)
+keymap(modes, "<Tab><Space>", "<cmd>HopChar2<CR>", hop_options)
 
-keymap("n", "<Tab>w", ":HopWordCurrentLine<CR>", hop_options)
-keymap("n", "<Tab>W", ":HopWord<CR>", hop_options)
-keymap("n", "<Tab><Tab>", ":HopWord<CR>", hop_options)
+keymap(modes, "<Tab>w", "<cmd>HopWordCurrentLine<CR>", hop_options)
+keymap(modes, "<Tab>W", "<cmd>HopWord<CR>", hop_options)
+keymap(modes, "<Tab><Tab>", "<cmd>HopWord<CR>", hop_options)
 
-keymap("n", "<Tab>h", ":HopAnywhereCurrentLineBC<CR>", hop_options)
-keymap("n", "<Tab>l", ":HopAnywhereCurrentLineAC<CR>", hop_options)
+keymap(modes, "<Tab>h", "<cmd>HopAnywhereCurrentLineBC<CR>", hop_options)
+keymap(modes, "<Tab>l", "<cmd>HopAnywhereCurrentLineAC<CR>", hop_options)
 
-keymap("n", "<Tab>k", ":HopLineBC<CR>", hop_options)
-keymap("n", "<Tab>j", ":HopLineAC<CR>", hop_options)
-keymap("n", "<Tab>0", ":HopLine<CR>", hop_options)
+keymap(modes, "<Tab>k", "<cmd>HopLineBC<CR>", hop_options)
+keymap(modes, "<Tab>j", "<cmd>HopLineAC<CR>", hop_options)
+keymap(modes, "<Tab>0", "<cmd>HopLine<CR>", hop_options)
+keymap(modes, "<Tab><CR>", "<cmd>HopLineMW<CR>", hop_options)
 
-keymap("n", "<Tab>g", ":HopAnywhere<CR>", hop_options)
-keymap("n", "<Tab>G", ":HopAnywhereMW<CR>", hop_options)
+keymap(modes, "<Tab>g", "<cmd>HopWordMW<CR>", hop_options)
 
-keymap("n", "<Tab>/", ":HopPattern<CR>", hop_options)
-keymap("n", "<Tab>f", ":HopPattern<CR>", hop_options)
+keymap(modes, "<Tab>/", "<cmd>HopPattern<CR>", hop_options)
+
+keymap(modes, "<Tab>f", "<cmd>HopChar1CurrentLineAC<CR>", hop_options)
+keymap(modes, "<Tab>F", "<cmd>HopChar1CurrentLineBC<CR>", hop_options)
+
+keymap(modes, "<Tab>t", "<cmd>HopChar2CurrentLineAC<CR>", hop_options)
+keymap(modes, "<Tab>T", "<cmd>HopChar2CurrentLineBC<CR>", hop_options)
+
+local hint = require "hop.hint"
+keymap(modes, "<Tab>e", function()
+    hop.hint_words({
+        hint_position = hint.HintPosition.END,
+        current_line_only = true,
+    })
+end, { silent = true, desc = "Hop end of word current line" })
+
+keymap(modes, "<Tab>E", function()
+    hop.hint_words({
+        hint_position = hint.HintPosition.END,
+    })
+end, { silent = true, desc = "Hop end of word global buffer" })
