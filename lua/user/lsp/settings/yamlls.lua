56edcb4638a82af232ec606f4f65127524e73757
ktunprasert <ktunprasert@outlook.com>
2022-07-05 17:40:16 +0100

    yamlls serverless yaml support

--- lua/user/lsp/settings/yamlls.lua
+++ lua/user/lsp/settings/yamlls.lua
@@ -1,13 +1,19 @@
 -- Find more schemas here: https://www.schemastore.org/json/
 local schemas = {
-    ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = "docker-compose.*",
+    ["https://raw.githubusercontent.com/compose-spec/compose-spec/master/schema/compose-spec.json"] = { "docker-compose.yaml", "docker-compose.yml" },
     ["https://gitlab.com/gitlab-org/gitlab/-/raw/master/app/assets/javascripts/editor/schema/ci.json"] = ".gitlab-ci.yml",
+    ["https://raw.githubusercontent.com/lalcebo/json-schema/master/serverless/reference.json"] = { "serverless.yaml", "serverless.yml" },
 }
 
 return {
     settings = {
         yaml = {
             schemas = schemas,
+            customtags = {
+                "!Ref",
+                "!GetAtt",
+                "!Sub"
+            }
         }
     }
 }
