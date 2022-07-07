dbffd25f1fd9858980ead104d3b17675fab641e2
ktunprasert <ktunprasert@outlook.com>
2022-07-07 20:26:47 +0100

    lazygit edit back in host nvim instance

--- lua/user/plugins/toggleterm.lua
+++ lua/user/plugins/toggleterm.lua
@@ -15,17 +15,41 @@ toggleterm.setup {
     open_mapping = [[<C-t>]],
     shading_factor = 2,
     float_opts = {
-      border = "curved",
-      winblend = 10,
-      highlights = {
-        border = "Normal",
-        background = "Normal",
-      },
+        border = "curved",
+        winblend = 10,
+        highlights = {
+            border = "Normal",
+            background = "Normal",
+        },
     },
 }
 
 local Terminal = require('toggleterm.terminal').Terminal
-local lazygit = Terminal:new({ cmd = "lg $(pwd)", hidden = true })
+
+local lg_cmd = "lg $(pwd)"
+if vim.v.servername ~= nil then
+    lg_cmd = string.format("NVIM_SERVER=%s lg $(pwd)", vim.v.servername)
+end
+
+local lazygit = Terminal:new({
+    count = 5,
+    cmd = lg_cmd,
+})
+
+-- For Editing back from LazyGit
+-- $ nvim --listen localhost:6666
+-- This WILL work even without the --listen option
+-- ~/.config/lazygit/config.yml
+-- os:
+--   editCommand: 'nvim --server $NVIM_SERVER'
+--   editCommandTemplate: "{{editor}} --remote-send '<C-\\><C-n>:5ToggleTerm<CR>:lua _edit({{filename}}, {{line}})<CR>'"
+function _edit(fn, line_number)
+    local edit_cmd = string.format(":e %s", fn)
+    if line_number ~= nil then
+        edit_cmd = string.format(":e +%d %s", line_number, fn)
+    end
+    vim.cmd(edit_cmd)
+end
 
 function _lazygit_toggle()
     lazygit:toggle()
