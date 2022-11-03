a998ce91a184c4a97bd498c47febef44ab612ee4
ktunprasert <ktunprasert@outlook.com>
2022-11-03 15:05:24 +0000

    prettier: enabled && no arguments

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -12,7 +12,7 @@ null_ls.setup({
     debug = false,
     sources = {
         -- TODO: Add Binaries so we can perform Formatting and Diagnostics
-        -- formatting.prettier.with({ extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" } }),
+        formatting.prettier.with({}),
         -- formatting.black.with({ extra_args = { "--fast" } }),
         -- formatting.stylua,
         -- diagnostics.flake8
