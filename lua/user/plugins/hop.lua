94ede04f626a76db260e2dc46a8707382cf298ac
ktunprasert <ktunprasert@outlook.com>
2022-12-07 12:00:55 +0000

    hop: give back my ^<Tab^> and ^<C-i^> (uncomment hop_leader)

--- lua/user/plugins/hop.lua
+++ lua/user/plugins/hop.lua
@@ -12,49 +12,52 @@ hop.setup {
 local hop_options = { silent = true }
 local modes = { "n", "v", "o" }
 
-keymap(modes, "<Tab>s", "<cmd>HopChar2<CR>", hop_options)
-keymap(modes, "<Tab>d", "<cmd>HopChar2<CR>", hop_options)
-keymap(modes, "<Tab><Space>", "<cmd>HopChar2<CR>", hop_options)
+-- local hop_leader = "<C-Space>"
+local hop_leader = "<Tab>"
 
-keymap(modes, "<Tab>w", "<cmd>HopWordCurrentLine<CR>", hop_options)
-keymap(modes, "<Tab>W", "<cmd>HopWord<CR>", hop_options)
-keymap(modes, "<Tab><Tab>", "<cmd>HopWord<CR>", hop_options)
+keymap(modes, hop_leader .. "s", "<cmd>HopChar2<CR>", hop_options)
+keymap(modes, hop_leader .. "d", "<cmd>HopChar2<CR>", hop_options)
+keymap(modes, hop_leader .. "<Space>", "<cmd>HopChar2<CR>", hop_options)
 
-keymap(modes, "<Tab>h", "<cmd>HopAnywhereCurrentLineBC<CR>", hop_options)
-keymap(modes, "<Tab>l", "<cmd>HopAnywhereCurrentLineAC<CR>", hop_options)
+keymap(modes, hop_leader .. "w", "<cmd>HopWordCurrentLine<CR>", hop_options)
+keymap(modes, hop_leader .. "W", "<cmd>HopWord<CR>", hop_options)
+keymap(modes, hop_leader .. hop_leader, "<cmd>HopWord<CR>", hop_options)
 
-keymap(modes, "<Tab>k", "<cmd>HopLineBC<CR>", hop_options)
-keymap(modes, "<Tab>j", "<cmd>HopLineAC<CR>", hop_options)
-keymap(modes, "<Tab>0", "<cmd>HopLine<CR>", hop_options)
-keymap(modes, "<Tab><CR>", "<cmd>HopLineMW<CR>", hop_options)
+keymap(modes, hop_leader .. "h", "<cmd>HopAnywhereCurrentLineBC<CR>", hop_options)
+keymap(modes, hop_leader .. "l", "<cmd>HopAnywhereCurrentLineAC<CR>", hop_options)
 
-keymap(modes, "<Tab>g", "<cmd>HopWordMW<CR>", hop_options)
+keymap(modes, hop_leader .. "k", "<cmd>HopLineBC<CR>", hop_options)
+keymap(modes, hop_leader .. "j", "<cmd>HopLineAC<CR>", hop_options)
+keymap(modes, hop_leader .. "0", "<cmd>HopLine<CR>", hop_options)
+keymap(modes, hop_leader .. "<CR>", "<cmd>HopLineMW<CR>", hop_options)
 
-keymap(modes, "<Tab>/", "<cmd>HopPattern<CR>", hop_options)
+keymap(modes, hop_leader .. "g", "<cmd>HopWordMW<CR>", hop_options)
 
-keymap(modes, "<Tab>f", "<cmd>HopChar1CurrentLineAC<CR>", hop_options)
-keymap(modes, "<Tab>F", "<cmd>HopChar1CurrentLineBC<CR>", hop_options)
+keymap(modes, hop_leader .. "/", "<cmd>HopPattern<CR>", hop_options)
 
-keymap(modes, "<Tab>t", "<cmd>HopChar2CurrentLineAC<CR>", hop_options)
-keymap(modes, "<Tab>T", "<cmd>HopChar2CurrentLineBC<CR>", hop_options)
+keymap(modes, hop_leader .. "f", "<cmd>HopChar1CurrentLineAC<CR>", hop_options)
+keymap(modes, hop_leader .. "F", "<cmd>HopChar1CurrentLineBC<CR>", hop_options)
+
+keymap(modes, hop_leader .. "t", "<cmd>HopChar2CurrentLineAC<CR>", hop_options)
+keymap(modes, hop_leader .. "T", "<cmd>HopChar2CurrentLineBC<CR>", hop_options)
 
 local hint = require "hop.hint"
 
-keymap(modes, "<Tab>$", function()
+keymap(modes, hop_leader .. "$", function()
     hop.hint_lines()
     vim.schedule(function()
         vim.cmd([[normal $]])
     end)
 end, hop_options)
 
-keymap(modes, "<Tab>e", function()
+keymap(modes, hop_leader .. "e", function()
     hop.hint_words({
         hint_position = hint.HintPosition.END,
         current_line_only = true,
     })
 end, { silent = true, desc = "Hop end of word current line" })
 
-keymap(modes, "<Tab>E", function()
+keymap(modes, hop_leader .. "E", function()
     hop.hint_words({
         hint_position = hint.HintPosition.END,
     })
