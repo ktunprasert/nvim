9ab04c49620bfe37d5422f724b0815cc4c526b6c
ktunprasert <ktunprasert@outlook.com>
2022-12-18 23:21:54 +0000

    navic: moved to plugins

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -242,46 +242,11 @@ return packer.startup(function(use)
     use {
         "SmiteshP/nvim-navic",
         requires = "neovim/nvim-lspconfig",
-        config = function()
-            vim.g.navic_silence = true
-            require("nvim-navic").setup {
-                icons = {
-                    File          = " ",
-                    Module        = " ",
-                    Namespace     = " ",
-                    Package       = " ",
-                    Class         = " ",
-                    Method        = " ",
-                    Property      = " ",
-                    Field         = " ",
-                    Constructor   = " ",
-                    Enum          = "練",
-                    Interface     = "練",
-                    Function      = " ",
-                    Variable      = " ",
-                    Constant      = " ",
-                    String        = " ",
-                    Number        = " ",
-                    Boolean       = "◩ ",
-                    Array         = " ",
-                    Object        = " ",
-                    Key           = " ",
-                    Null          = "ﳠ ",
-                    EnumMember    = " ",
-                    Struct        = " ",
-                    Event         = " ",
-                    Operator      = " ",
-                    TypeParameter = " ",
-                },
-                highlight = false,
-                separator = " > ",
-                depth_limit = 0,
-                depth_limit_indicator = "..",
-                safe_output = true
-            }
-            vim.o.winbar = "%{%v:lua.require'nvim-navic'.get_location()%}"
-        end,
+        config = function() require("user.plugins.navic") end,
     }
+
+        config = function()
+        end,
     -- ███╗   ██╗ █████╗ ██╗   ██╗██╗ ██████╗  █████╗ ████████╗██╗ ██████╗ ███╗   ██╗
     -- ████╗  ██║██╔══██╗██║   ██║██║██╔════╝ ██╔══██╗╚══██╔══╝██║██╔═══██╗████╗  ██║
     -- ██╔██╗ ██║███████║██║   ██║██║██║  ███╗███████║   ██║   ██║██║   ██║██╔██╗ ██║
---
+++ lua/user/plugins/navic.lua
@@ -0,0 +1,41 @@
+local navic_ok, navic = pcall(require, "nvim-navic")
+if not navic_ok then
+    return
+end
+vim.g.navic_silence = true
+navic.setup {
+    icons = {
+        File          = " ",
+        Module        = " ",
+        Namespace     = " ",
+        Package       = " ",
+        Class         = " ",
+        Method        = " ",
+        Property      = " ",
+        Field         = " ",
+        Constructor   = " ",
+        Enum          = "練",
+        Interface     = "練",
+        Function      = " ",
+        Variable      = " ",
+        Constant      = " ",
+        String        = " ",
+        Number        = " ",
+        Boolean       = "◩ ",
+        Array         = " ",
+        Object        = " ",
+        Key           = " ",
+        Null          = "ﳠ ",
+        EnumMember    = " ",
+        Struct        = " ",
+        Event         = " ",
+        Operator      = " ",
+        TypeParameter = " ",
+    },
+    highlight = false,
+    separator = " > ",
+    depth_limit = 0,
+    depth_limit_indicator = "..",
+    safe_output = true
+}
+vim.o.winbar = "%{%v:lua.require'nvim-navic'.get_location()%}"
