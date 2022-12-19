204e988a7b53e0a1948c8f8c4769bebb1fe2e2a8
ktunprasert <ktunprasert@outlook.com>
2022-12-19 00:05:54 +0000

    noice: disable noice signature

--- lua/user/plugins/noice.lua
+++ lua/user/plugins/noice.lua
@@ -5,6 +5,9 @@ end
 
 noice.setup({
     lsp = {
+        signature = {
+            enabled = false,
+        },
         -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
         override = {
             ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
@@ -18,6 +21,6 @@ noice.setup({
         command_palette = true, -- position the cmdline and popupmenu together
         long_message_to_split = true, -- long messages will be sent to a split
         inc_rename = false, -- enables an input dialog for inc-rename.nvim
-        lsp_doc_border = false, -- add a border to hover docs and signature help
+        lsp_doc_border = true, -- add a border to hover docs and signature help
     },
 })
