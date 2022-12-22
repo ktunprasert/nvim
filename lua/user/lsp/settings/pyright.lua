f4820a1a4d6fb3d09da40b54f866623284de0760
ktunprasert <ktunprasert@outlook.com>
2022-12-22 18:13:48 +0000

    pyright: added pyright config

---
+++ lua/user/lsp/settings/pyright.lua
@@ -0,0 +1,11 @@
+return {
+    settings = {
+        python = {
+            analysis = {
+                diagnosticSeverityOverrides = {
+                    reportGeneralTypeIssues = "none",
+                }
+            }
+        }
+    }
+}
