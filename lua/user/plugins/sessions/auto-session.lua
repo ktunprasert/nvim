cf1394df59a2de1c1f32814946a77441841f0324
ktunprasert <ktunprasert@outlook.com>
2023-01-11 14:32:28 +0000

    autosession: enable last session from root

--- lua/user/plugins/sessions/auto-session.lua
+++ lua/user/plugins/sessions/auto-session.lua
@@ -5,7 +5,6 @@ end
 
 auto_session.setup {
     log_level = 'info',
-    auto_session_suppress_dirs = {'~/'},
-    pre_save_cmds = {"NeoTreeClose"}
+    pre_save_cmds = { "NeoTreeClose" },
+    auto_session_enable_last_session = true,
 }
-
