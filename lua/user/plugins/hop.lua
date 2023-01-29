13bec0ea3e9b53207f2a77e5330a98eadb7586f9
ktunprasert <ktunprasert@outlook.com>
2023-01-29 21:18:14 +0000

    hop: added hop by treesitter

--- lua/user/plugins/hop.lua
+++ lua/user/plugins/hop.lua
@@ -3,6 +3,11 @@ if not ok then
     return
 end
 
+local sok, sts = pcall(require, "syntax-tree-surfer")
+if not sok then
+    return
+end
+
 local keymap = require 'lib.utils'.keymap
 
 hop.setup {
@@ -12,11 +17,16 @@ hop.setup {
 local hop_options = { silent = true }
 local modes = { "n", "v", "o" }
 
+
 local hop_leader = "<C-Space>"
 -- local hop_leader = "<Tab>"
 
-function hop_keymap(km, exec)
-    return keymap(modes, hop_leader .. km, exec, hop_options)
+local function hop_keymap(km, exec, opts)
+    local _opts = hop_options
+    if opts then
+        _opts = vim.tbl_deep_extend("force", hop_options, opts)
+    end
+    return keymap(modes, hop_leader .. km, exec, _opts)
 end
 
 hop_keymap("s", "<cmd>HopChar2<CR>")
@@ -39,11 +49,46 @@ hop_keymap("g", "<cmd>HopWordMW<CR>")
 
 hop_keymap("/", "<cmd>HopPattern<CR>")
 
-hop_keymap("f", "<cmd>HopChar1CurrentLineAC<CR>")
-hop_keymap("F", "<cmd>HopChar1CurrentLineBC<CR>")
+hop_keymap("a", function()
+    sts.targeted_jump({
+        "arguments",
+        "field"
+    })
+end, { desc = "Hop to closest field or arguments" })
 
-hop_keymap("t", "<cmd>HopChar2CurrentLineAC<CR>")
-hop_keymap("T", "<cmd>HopChar2CurrentLineBC<CR>")
+hop_keymap("c", function()
+    sts.targeted_jump({
+        "identifier",
+        "comment",
+        "chunk"
+    })
+end, { desc = "Hop to closest node or comment" })
+
+hop_keymap("v", function()
+    sts.targeted_jump({
+        "variable_declaration"
+    })
+end, { desc = "Hop to closest variable" })
+
+hop_keymap("f", function()
+    sts.targeted_jump({
+        "function",
+        "arrrow_function",
+        "function_definition"
+    })
+end, { desc = "Hop to closest function" })
+
+hop_keymap("i", function()
+    sts.targeted_jump({
+        "if_statement",
+        "else_cause",
+        "else_statement",
+        "elseif_statement",
+        "for_statement",
+        "while_statement",
+        "switch_statement"
+    })
+end, { desc = "Hop to closest control block" })
 
 local hint = require "hop.hint"
 
