2045d260b298ea7be5b41a82773207bcbc6cc454
ktunprasert <ktunprasert@outlook.com>
2023-01-29 23:22:22 +0000

    gitsigns: brevity

--- lua/user/plugins/gitsigns.lua
+++ lua/user/plugins/gitsigns.lua
@@ -10,29 +10,30 @@ gitsigns.setup {
     numhl = true,
 }
 
+local _opts = { expr = true }
+
 local gs = package.loaded.gitsigns
 
 keymap('n', ']c', function()
     if vim.wo.diff then return ']c' end
     vim.schedule(function() gs.next_hunk() end)
     return '<Ignore>'
-end, {expr=true})
+end, _opts)
 
 keymap('n', '[c', function()
     if vim.wo.diff then return '[c' end
     vim.schedule(function() gs.prev_hunk() end)
     return '<Ignore>'
-end, {expr=true})
+end, _opts)
 
 keymap('n', ']g', function()
     if vim.wo.diff then return ']g' end
     vim.schedule(function() gs.next_hunk() end)
     return '<Ignore>'
-end, {expr=true})
+end, _opts)
 
 keymap('n', '[g', function()
     if vim.wo.diff then return '[g' end
     vim.schedule(function() gs.prev_hunk() end)
     return '<Ignore>'
-end, {expr=true})
-
+end, _opts)
