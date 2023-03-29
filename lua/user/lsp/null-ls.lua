edc66049ac75adcdfa14af5745b245368969754f
ktunprasert <ktunprasert@outlook.com>
2023-03-29 18:36:00 +0100

    null-ls: added v fmt

--- lua/user/lsp/null-ls.lua
+++ lua/user/lsp/null-ls.lua
@@ -8,14 +8,32 @@ local formatting = null_ls.builtins.formatting
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 local diagnostics = null_ls.builtins.diagnostics
 
+local h = require("null-ls.helpers")
+local FMT = require("null-ls.methods").internal.FORMATTING
+
 null_ls.setup({
     debug = false,
     sources = {
         -- TODO: Add Binaries so we can perform Formatting and Diagnostics
-        formatting.prettier.with({}),
-        -- formatting.black.with({ extra_args = { "--fast" } }),
-        formatting.prettier.with({ extra_args = {}}),
+        formatting.prettier.with({ extra_args = {} }),
+        formatting.black.with({ extra_args = { "--fast" } }),
+        -- formatting.blue.with({ extra_args = { "--fast" } }),
+        -- formatting.yamlfmt.with({ extra_args = { "-conf=~/.config/.yamlfmt" } }),
         -- formatting.stylua,
         -- diagnostics.flake8
+        -- null_ls.builtins.code_actions.gitsigns,
+        h.make_builtin(
+            {
+                name = "v fmt",
+                method = FMT,
+                filetypes = { "v" },
+                generator_opts = {
+                    command = "v",
+                    args = { "fmt" },
+                    to_stdin = true,
+                },
+                factory = h.formatter_factory,
+            }
+        ),
     },
 })
