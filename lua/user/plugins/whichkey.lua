7f49beea192089e29b6d4770261ba38bd9d33413
ktunprasert <ktunprasert@outlook.com>
2023-08-17 21:30:32 +0100

    whichkey: formatted and use <cmd> instead of colon

--- lua/user/plugins/whichkey.lua
+++ lua/user/plugins/whichkey.lua
@@ -6,47 +6,47 @@ end
 
 whichkey.setup {
     plugins = {
-        marks = true, -- shows a list of your marks on ' and `
+        marks = true,     -- shows a list of your marks on ' and `
         registers = true, -- shows your registers on " in NORMAL or <C-r> in INSERT mode
         -- the presets plugin, adds help for a bunch of default keybindings in Neovim
         -- No actual key bindings are created
         presets = {
-            operators = false, -- adds help for operators like d, y, ...
-            motions = false, -- adds help for motions
-            text_objects = false, -- help for text objects triggered after entering an operator
-            windows = true, -- default bindings on <c-w>
-            nav = true, -- misc bindings to work with windows
-            z = true, -- bindings for folds, spelling and others prefixed with z
-            g = true, -- bindings for prefixed with g
+            operators = false,                           -- adds help for operators like d, y, ...
+            motions = true,                              -- adds help for motions
+            text_objects = true,                         -- help for text objects triggered after entering an operator
+            windows = true,                              -- default bindings on <c-w>
+            nav = true,                                  -- misc bindings to work with windows
+            z = true,                                    -- bindings for folds, spelling and others prefixed with z
+            g = true,                                    -- bindings for prefixed with g
         },
         spelling = { enabled = true, suggestions = 20 }, -- use which-key for spelling hints
     },
     icons = {
         breadcrumb = "»", -- symbol used in the command line area that shows your active key combo
-        separator = "-", -- symbol used between a key and it's label
-        group = "+", -- symbol prepended to a group
+        separator = "-",  -- symbol used between a key and it's label
+        group = "+",      -- symbol prepended to a group
     },
     popup_mappings = {
         scroll_down = "<c-d>", -- binding to scroll down inside the popup
-        scroll_up = "<c-u>", -- binding to scroll up inside the popup
+        scroll_up = "<c-u>",   -- binding to scroll up inside the popup
     },
     window = {
-        border = "none", -- none, single, double, shadow
-        position = "bottom", -- bottom, top
-        margin = { 0, 0, 0, 0 }, -- extra window margin [top, right, bottom, left]
+        border = "none",          -- none, single, double, shadow
+        position = "bottom",      -- bottom, top
+        margin = { 0, 0, 0, 0 },  -- extra window margin [top, right, bottom, left]
         padding = { 1, 1, 1, 1 }, -- extra window padding [top, right, bottom, left]
         winblend = 0,
     },
     layout = {
-        height = { min = 4, max = 25 }, -- min and max height of the columns
-        width = { min = 20, max = 50 }, -- min and max width of the columns
-        spacing = 3, -- spacing between columns
-        align = "center", -- align columns left, center or right
+        height = { min = 4, max = 25 },                                           -- min and max height of the columns
+        width = { min = 20, max = 50 },                                           -- min and max width of the columns
+        spacing = 3,                                                              -- spacing between columns
+        align = "center",                                                         -- align columns left, center or right
     },
     hidden = { "<silent>", "<cmd>", "<Cmd>", "<CR>", "call", "lua", "^:", "^ " }, -- hide mapping boilerplate
-    ignore_missing = false, -- enable this to hide mappings for which you didn't specify a label
-    show_help = true, -- show help message on the command line when the popup is visible
-    triggers = "auto", -- automatically setup triggers
+    ignore_missing = false,                                                       -- enable this to hide mappings for which you didn't specify a label
+    show_help = true,                                                             -- show help message on the command line when the popup is visible
+    triggers = "auto",                                                            -- automatically setup triggers
     -- triggers = {"<leader>"} -- or specify a list manually
     triggers_blacklist = {
         -- list of mode / prefixes that should never be hooked by WhichKey
@@ -76,7 +76,7 @@ local vopts = {
 }
 
 local vmappings = {
-    ["c"] = { "<Esc><cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>", "Comment" },
+    ["c"] = { "<Esc><Cmd>lua require('Comment.api').toggle.linewise(vim.fn.visualmode())<CR>", "Comment" },
 }
 
 local mappings = {
@@ -87,44 +87,44 @@ local mappings = {
     Q = { "<Cmd>qa<CR>", "Quit" },
     l = { "<Cmd>Lazy<CR>", "Lazy" },
     m = { "<Cmd>Mason<CR>", "Mason" },
-    ["c"] = { "<Esc><cmd>lua require('Comment.api').toggle.linewise()<CR>", "Comment" },
-    ["f"] = { ":NeoTreeReveal<CR>", "Find Files" },
-    F = { ":Format<CR>", "Format current file" },
-    ["p"] = { ":Telescope projects<CR>", "Projects" },
-    r = { ":Telescope oldfiles<CR>", "Recent Files" },
-    d = { "<cmd>bp | sp | bn | bd<CR>", "Delete Buffer" },
-    D = { "<cmd>%bd<CR>", "Delete ALL Buffers" },
+    ["c"] = { "<Esc><Cmd>lua require('Comment.api').toggle.linewise()<CR>", "Comment" },
+    ["f"] = { "<Cmd>NeoTreeReveal<CR>", "Find Files" },
+    F = { "<Cmd>Format<CR>", "Format current file" },
+    ["p"] = { "<Cmd>Telescope projects<CR>", "Projects" },
+    r = { "<Cmd>Telescope oldfiles<CR>", "Recent Files" },
+    d = { "<Cmd>bp | sp | bn | bd<CR>", "Delete Buffer" },
+    D = { "<Cmd>%bd<CR>", "Delete ALL Buffers" },
     G = { name = "LazyGit" },
     g = {
         name = "Git",
-        g = { ":Telescope git_files<CR>", "Git Files" },
-        h = { ":Gitsigns preview_hunk<CR>", "Preview Hunk" },
-        H = { "<cmd>Telescope git_bcommits<cr>", "View Git History for current buffer" },
-        c = { ":Telescope git_commits<CR>", "View Commits" },
-        b = { ":Telescope git_branches<CR>", "View Branches" },
-        s = { ":Gitsigns setqflist<CR>", "View Hunks" },
-        ['!'] = { ":Gitsigns reset_buffer<CR>", "Reset buffer to revision" },
-        r = { ":Gitsigns reset_hunk<CR>", "Reset Hunk" },
+        g = { "<Cmd>Telescope git_files<CR>", "Git Files" },
+        h = { "<Cmd>Gitsigns preview_hunk<CR>", "Preview Hunk" },
+        H = { "<Cmd>Telescope git_bcommits<cr>", "View Git History for current buffer" },
+        c = { "<Cmd>Telescope git_commits<CR>", "View Commits" },
+        b = { "<Cmd>Telescope git_branches<CR>", "View Branches" },
+        s = { "<Cmd>Gitsigns setqflist<CR>", "View Hunks" },
+        ['!'] = { "<Cmd>Gitsigns reset_buffer<CR>", "Reset buffer to revision" },
+        r = { "<Cmd>Gitsigns reset_hunk<CR>", "Reset Hunk" },
     },
     s = {
         name = "Search [Telescope]",
-        g = { ":Telescope git_files<CR>", "Git Files" },
-        f = { ":Telescope find_files<CR>", "Find All Files" },
-        t = { "<cmd>Telescope live_grep<cr>", "Text" },
-        h = { "<cmd>Telescope help_tags<cr>", "Find Help" },
-        b = { ":Telescope buffers<CR>", "Buffers" },
-        k = { ":Telescope keymaps<CR>", "Keymaps" },
-        ['"'] = { ":Telescope registers<CR>", "Registers" },
-        p = { [[<cmd>lua require'telescope.builtin'.colorscheme({enable_preview = true})<CR>]], "Colourscheme Preview" },
-        s = { ':Telescope session-lens search_session<CR>', "Sessions" },
-        o = { ":Telescope lsp_document_symbols<CR>", "Document Symbols" },
-        O = { ":Telescope lsp_workspace_symbols<CR>", "Workspace Symbols" },
-        r = { ":Telescope lsp_references<CR>", "References" },
-        c = { ":Telescope commands<CR>", "Commands" },
-        d = { ":Telescope diagnostics bufnr=0<CR>", "Diagnostics current buffer" },
-        D = { ":Telescope diagnostics<CR>", "Global diagnostics" },
+        g = { "<Cmd>Telescope git_files<CR>", "Git Files" },
+        f = { "<Cmd>Telescope find_files<CR>", "Find All Files" },
+        t = { "<Cmd>Telescope live_grep<cr>", "Text" },
+        h = { "<Cmd>Telescope help_tags<cr>", "Find Help" },
+        b = { "<Cmd>Telescope buffers<CR>", "Buffers" },
+        k = { "<Cmd>Telescope keymaps<CR>", "Keymaps" },
+        ['"'] = { "<Cmd>Telescope registers<CR>", "Registers" },
+        p = { [[<Cmd>lua require'telescope.builtin'.colorscheme({enable_preview = true})<CR>]], "Colourscheme Preview" },
+        s = { "<Cmd>Telescope session-lens search_session<CR>", "Sessions" },
+        o = { "<Cmd>Telescope lsp_document_symbols<CR>", "Document Symbols" },
+        O = { "<Cmd>Telescope lsp_workspace_symbols<CR>", "Workspace Symbols" },
+        r = { "<Cmd>Telescope lsp_references<CR>", "References" },
+        c = { "<Cmd>Telescope commands<CR>", "Commands" },
+        d = { "<Cmd>Telescope diagnostics bufnr=0<CR>", "Diagnostics current buffer" },
+        D = { "<Cmd>Telescope diagnostics<CR>", "Global diagnostics" },
     },
-    ["/"] = { ":HopPattern<CR>", "Hop by Pattern" },
+    ["/"] = { "<Cmd>HopPattern<CR>", "Hop by Pattern" },
     b = {
         d = { "<Cmd>bd!<CR>", "Force delete buffer" },
     },
