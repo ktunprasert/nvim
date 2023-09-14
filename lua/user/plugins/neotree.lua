578ba2c25c7db2e3cbe13ee8e40eea36cde02b7b
ktunprasert <ktunprasert@outlook.com>
2023-09-14 20:11:01 +0100

    neotree: stop following file cwd

--- lua/user/plugins/neotree.lua
+++ lua/user/plugins/neotree.lua
@@ -123,6 +123,7 @@ neotree.setup {
                 --"thumbs.db"
             },
         },
+        bind_to_cwd = false,
         follow_current_file = false, -- This will find and focus the file in the active buffer every
         -- time the current file is changed while the tree is open.
         group_empty_dirs = false, -- when true, empty folders will be grouped together
