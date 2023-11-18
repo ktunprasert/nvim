d78de0d14ca30d64df844062e5844af4055577ca
ktunprasert <ktunprasert@outlook.com>
2023-11-18 18:22:31 +0000

    emmet: inclusive

--- lua/user/lsp/settings/emmet_ls.lua
+++ lua/user/lsp/settings/emmet_ls.lua
@@ -1,5 +1,18 @@
 return {
     filetypes = {
+        "astro",
+        "css",
+        "eruby",
+        "html",
+        "htmldjango",
+        "javascriptreact",
+        "less",
+        "pug",
+        "sass",
+        "scss",
+        "svelte",
+        "typescriptreact",
+        "vue",
         "heex",
         "eex",
         "elixir",
