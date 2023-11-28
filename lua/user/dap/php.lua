244a37a23dc10aed397a1fcebe535b70e63acc97
ktunprasert <ktunprasert@outlook.com>
2023-11-28 21:50:21 +0000

    dap.php: use MASON_PATH

--- lua/user/dap/php.lua
+++ lua/user/dap/php.lua
@@ -3,7 +3,7 @@ local dap = require("dap")
 dap.adapters.php = {
     type = 'executable',
     command = 'node',
-    args = { os.getenv("HOME") .. "/build/vscode-php-debug/out/phpDebug.js" }
+    args = { MASON_PATH .. "/php-debug-adapter/extension/out/phpDebug.js" }
 }
 
 dap.configurations.php = {
