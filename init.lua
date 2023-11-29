11c268dd05e3706cfb752f4ed1c171e45d7df5e3
ktunprasert <ktunprasert@outlook.com>
2023-11-29 15:43:22 +0000

    global: load before everything

--- init.lua
+++ init.lua
@@ -1,10 +1,10 @@
 -- Global neovim dotfiles for configuring exactly your Neovim
 --
 -- User settings
+require "user.globals"
 require "user.options"
 require "user.keymaps"
 require "user.lazy"
 require "user.cmp"
 require "user.lsp.init"
 require "user.abbreviations"
-require "user.globals"
