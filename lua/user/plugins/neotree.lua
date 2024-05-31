49f64720c89ea1eb49f9bc4be36c1b1194b04099
ktunprasert <ktunprasert@outlook.com>
2024-05-31 18:57:41 +0100

    neotree: migrated configs from v2.x

--- lua/user/plugins/neotree.lua
+++ lua/user/plugins/neotree.lua
@@ -124,10 +124,11 @@ neotree.setup {
             },
         },
         bind_to_cwd = false,
-        follow_current_file = false, -- This will find and focus the file in the active buffer every
+        -- follow_current_file = false, -- This will find and focus the file in the active buffer every
+        follow_current_file = { enabled = false }, -- This will find and focus the file in the active buffer every
         -- time the current file is changed while the tree is open.
-        group_empty_dirs = false, -- when true, empty folders will be grouped together
-        hijack_netrw_behavior = "open_default", -- netrw disabled, opening a directory opens neo-tree
+        group_empty_dirs = false,                  -- when true, empty folders will be grouped together
+        hijack_netrw_behavior = "open_default",    -- netrw disabled, opening a directory opens neo-tree
         -- in whatever position is specified in window.position
         -- "open_current",  -- netrw disabled, opening a directory opens within the
         -- window like netrw would, regardless of window.position
@@ -150,9 +151,9 @@ neotree.setup {
     },
     buffers = {
         bind_to_cwd = false,
-        follow_current_file = false, -- This will find and focus the file in the active buffer every
+        follow_current_file = { enabled = false }, -- This will find and focus the file in the active buffer every
         -- time the current file is changed while the tree is open.
-        group_empty_dirs = true, -- when true, empty folders will be grouped together
+        group_empty_dirs = true,                  -- when true, empty folders will be grouped together
         show_unloaded = true,
         window = {
             mappings = {
