3477d9feb0063a67e1247da1e5d8fd8a6e0a9f30
ktunprasert <ktunprasert@outlook.com>
2025-03-25 21:59:19 +0000

    chore: fmt

--- lua/user/lazy.lua
+++ lua/user/lazy.lua
@@ -122,12 +122,11 @@ require("lazy").setup({
             "nvim-lua/plenary.nvim",
             "MunifTanjim/nui.nvim",
             --- The below dependencies are optional,
-            "echasnovski/mini.pick", -- for file_selector provider mini.pick
+            "echasnovski/mini.pick",         -- for file_selector provider mini.pick
             "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
-            "hrsh7th/nvim-cmp",      -- autocompletion for avante commands and mentions
-            "ibhagwan/fzf-lua",      -- for file_selector provider fzf
-            "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-            "zbirenbaum/copilot.lua", -- for providers='copilot'
+            "hrsh7th/nvim-cmp",              -- autocompletion for avante commands and mentions
+            "ibhagwan/fzf-lua",              -- for file_selector provider fzf
+            "nvim-tree/nvim-web-devicons",   -- or echasnovski/mini.icons
             {
                 -- support for image pasting
                 "HakonHarnes/img-clip.nvim",
