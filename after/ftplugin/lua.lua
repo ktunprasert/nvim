7419538fbe564282b175606b6d89ff04a6ae3e15
ktunprasert <ktunprasert@outlook.com>
2025-04-02 22:19:45 +0100

    feat(lua): format on save unless within plugins folder

--- after/ftplugin/lua.lua
+++ after/ftplugin/lua.lua
@@ -1,6 +1,11 @@
 vim.opt.shiftwidth = 4
 
 vim.api.nvim_create_autocmd("BufWritePre", {
-    pattern = { "*.lua" },
-    command = "Format",
+    callback = function(tbl)
+        if tbl.match:match("/.local/share/nvim/") then
+            return false
+        end
+
+        vim.lsp.buf.format()
+    end,
 })
