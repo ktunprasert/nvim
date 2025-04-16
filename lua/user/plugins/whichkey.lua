a7106466478c123c833dfa9a395471c127f9e0b2
ktunprasert <ktunprasert@outlook.com>
2025-04-16 15:43:18 +0100

    feat(whichkey): show operator stuff because various-textobjs

--- lua/user/plugins/whichkey.lua
+++ lua/user/plugins/whichkey.lua
@@ -11,7 +11,7 @@ whichkey.setup {
         -- the presets plugin, adds help for a bunch of default keybindings in Neovim
         -- No actual key bindings are created
         presets = {
-            operators = false,                           -- adds help for operators like d, y, ...
+            operators = true,                            -- adds help for operators like d, y, ...
             motions = true,                              -- adds help for motions
             text_objects = true,                         -- help for text objects triggered after entering an operator
             windows = true,                              -- default bindings on <c-w>
