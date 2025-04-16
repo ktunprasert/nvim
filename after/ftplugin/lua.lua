655cd88aba3fdbbe639cf3c31aa3fbff43f2e0ab
ktunprasert <ktunprasert@outlook.com>
2025-04-16 20:42:09 +0100

    feat(lua): source shortcut F6

--- after/ftplugin/lua.lua
+++ after/ftplugin/lua.lua
@@ -1,4 +1,9 @@
+local keymap = require('lib.utils').keymap
+
 vim.opt.shiftwidth = 4
+
+-- source current file
+keymap("n", "<F6>", "<cmd>source<CR>")
 
 vim.api.nvim_create_autocmd("BufWritePre", {
     callback = function(tbl)
