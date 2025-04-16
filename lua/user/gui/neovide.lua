e7335859b14372222730ece63ddb7781e28ead7c
ktunprasert <ktunprasert@outlook.com>
2025-04-16 22:23:02 +0100

    feat(neovide): fuckin clipboard solved

--- lua/user/gui/neovide.lua
+++ lua/user/gui/neovide.lua
@@ -1,3 +1,6 @@
-vim.opt.clipboard = ""
 vim.opt.fileformat = "unix"
 vim.g.neovide_refresh_rate = 144
+vim.keymap.set('n', '<C-v>', '"+P')         -- Paste normal mode
+vim.keymap.set('v', '<C-v>', '"+P')         -- Paste visual mode
+vim.keymap.set('c', '<C-v>', '<C-R>+')      -- Paste command mode
+vim.keymap.set('i', '<C-v>', '<ESC>l"+Pli') -- Paste insert mode
