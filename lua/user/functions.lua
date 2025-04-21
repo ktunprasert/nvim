e68d71583d6b66aea6bcf2bdab61dba3ecffaa33
ktunprasert <ktunprasert@outlook.com>
2025-04-21 21:12:02 +0100

    feat(parent): smarter by trying to go left once and traverse

--- lua/user/functions.lua
+++ lua/user/functions.lua
@@ -1,12 +1,13 @@
 -- Treesitter functions
 local M = {}
 
--- Function to traverse up the TreeSitter node hierarchy
 M.ts_parent_node = function()
-    -- print("hi")
     local ts_utils = require('nvim-treesitter.ts_utils')
-    local current_node = ts_utils.get_node_at_cursor()
 
+    -- Get current cursor position before any movement
+    local initial_pos = vim.api.nvim_win_get_cursor(0)
+
+    local current_node = ts_utils.get_node_at_cursor()
     if not current_node then
         print("No treesitter node found at cursor position")
         return
@@ -21,8 +22,19 @@ M.ts_parent_node = function()
     -- Move cursor to the parent node
     ts_utils.goto_node(parent)
 
-    -- Optionally highlight the node
-    -- ts_utils.update_selection(0, parent)
+    -- Check if cursor position has changed
+    local new_pos = vim.api.nvim_win_get_cursor(0)
+    if new_pos[1] == initial_pos[1] and new_pos[2] == initial_pos[2] then
+        -- Cursor didn't move, try going left by one and try again
+        local col = initial_pos[2]
+        if col > 0 then
+            -- Move cursor left by one
+            vim.api.nvim_win_set_cursor(0, { initial_pos[1], col - 1 })
+
+            -- Try the function again recursively
+            return M.ts_parent_node()
+        end
+    end
 
     -- Return the parent node in case further processing is needed
     return parent
