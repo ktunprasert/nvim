f5d6e8bf84bef0246221dabb8f57059174336d30
ktunprasert <ktunprasert@outlook.com>
2025-04-22 09:11:16 +0100

    fix(multicursor): c-l is reserved for switching windows oops

--- lua/user/lazy/util.lua
+++ lua/user/lazy/util.lua
@@ -188,7 +188,7 @@ return {
 
             -- Add or skip adding a new cursor by matching word/selection
             set({ "n", "x" }, "<C-n>", function() mc.matchAddCursor(1) end)
-            set({ "n", "x" }, "<C-l>", function() mc.matchAddCursor(1) end)
+            -- set({ "n", "x" }, "<C-l>", function() mc.matchAddCursor(1) end)
             -- set({ "n", "x" }, "<leader>s", function() mc.matchSkipCursor(1) end)
 
             -- Add and remove cursors with control + left click.
