f0500c65ae563c28a2f166cfda7c21956c1a63b2
ktunprasert <ktunprasert@outlook.com>
2025-04-23 23:41:12 +0100

    feat(keymaps): implement ycc keybinding for yank, comment, and paste operation

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -33,8 +33,7 @@ keymap("n", "<C-s>", ":w<CR>")
 keymap("", "gf", ":e <cfile><CR>")
 
 -- Command mode without shift
-keymap("n", ";", ":")
-keymap("v", ";", ":")
+keymap({ "n", "v" }, ";", ":")
 
 -- Replay macro
 keymap("n", ",,", "@@")
@@ -171,3 +170,42 @@ keymap("n", "<F3>", function() require("fastaction").code_action() end)
 keymap("n", "gh", "<cmd>lua vim.diagnostic.open_float()<CR>")
 keymap("n", "<Leader>q", "<cmd>lua vim.diagnostic.setloclist()<CR>")
 keymap("n", "gl", '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics({ border = "rounded" })<CR>')
+
+-- yoink
+-- https://www.reddit.com/r/neovim/comments/1k4efz8/share_your_proudest_config_oneliners/mo9t5xq/
+-- yank comment paste
+-- keymap("n", "ycc", "yygccp", { remap = true })
+-- improved with expression
+-- https://www.reddit.com/r/neovim/comments/1k4efz8/share_your_proudest_config_oneliners/mob2hwx/
+-- keymap("n", "ycc", '"yy" . v:count1 . "gcc\']p"', { remap = true, expr = true })
+-- improved again...
+-- https://www.reddit.com/r/neovim/comments/1k4efz8/share_your_proudest_config_oneliners/mola3k0/
+-- Duplicate selection and comment out the first instance.
+function _G.duplicate_and_comment_lines()
+    local start_line, end_line = vim.api.nvim_buf_get_mark(0, '[')[1], vim.api.nvim_buf_get_mark(0, ']')[1]
+
+    -- NOTE: `nvim_buf_get_mark()` is 1-indexed, but `nvim_buf_get_lines()` is 0-indexed. Adjust accordingly.
+    local lines = vim.api.nvim_buf_get_lines(0, start_line - 1, end_line, false)
+
+    -- Store cursor position because it might move when commenting out the lines.
+    local cursor = vim.api.nvim_win_get_cursor(0)
+
+    -- Comment out the selection using the builtin gc operator.
+    vim.cmd.normal({ 'gcc', range = { start_line, end_line } })
+
+    -- Append a duplicate of the selected lines to the end of selection.
+    vim.api.nvim_buf_set_lines(0, end_line, end_line, false, lines)
+
+    -- Move cursor to the start of the duplicate lines.
+    vim.api.nvim_win_set_cursor(0, { end_line + 1, cursor[2] })
+end
+
+vim.keymap.set('n', 'yc', function()
+    vim.opt.operatorfunc = 'v:lua.duplicate_and_comment_lines'
+    return 'g@'
+end, { expr = true, desc = 'Duplicate selection and comment out the first instance' })
+
+vim.keymap.set('n', 'ycc', function()
+    vim.opt.operatorfunc = 'v:lua.duplicate_and_comment_lines'
+    return 'g@_'
+end, { expr = true, desc = 'Duplicate [count] lines and comment out the first instance' })
