2e0838b2f1b45eed2688d1c2da7b9e9e3cd4bddc
ktunprasert <ktunprasert@outlook.com>
2025-04-24 20:43:27 +0100

    fix(lsp): correct diagnostic disabling for fish LSP

--- lua/user/lsp/settings/fish_lsp.lua
+++ lua/user/lsp/settings/fish_lsp.lua
@@ -1,5 +1,7 @@
 return {
     cmd_env = {
-        fish_lsp_diagnostic_disable_error_codes = { 2002 }
-    }
+        -- this is a fish array so single arg command is better than a table
+        -- e.g. fish_lsp_diagnostic_disable_error_codes = "2002 1001",
+        fish_lsp_diagnostic_disable_error_codes = "2002",
+    },
 }
