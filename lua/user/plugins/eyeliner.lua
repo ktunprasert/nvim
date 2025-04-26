d43a252c88af418c77647823ba216e435ae79302
ktunprasert <ktunprasert@outlook.com>
2025-04-26 02:08:07 +0100

    fix(eyeliner): dont pollute autocmd

--- lua/user/plugins/eyeliner.lua
+++ lua/user/plugins/eyeliner.lua
@@ -1,4 +1,4 @@
-local comment_color = vim.api.nvim_get_hl(0, {name = 'Comment', link = false})
+local comment_color = vim.api.nvim_get_hl(0, { name = 'Comment', link = false })
 
 local opts = {
     primary = {
@@ -11,10 +11,10 @@ local opts = {
 vim.api.nvim_set_hl(0, 'EyelinerPrimary', opts.primary)
 vim.api.nvim_set_hl(0, 'EyelinerSecondary', {})
 
-vim.api.nvim_create_autocmd('ColorScheme', {
-    pattern = '*',
-    callback = function()
-        vim.api.nvim_set_hl(0, 'EyelinerPrimary', opts.primary)
-        vim.api.nvim_set_hl(0, 'EyelinerSecondary', {})
-    end,
-})
+-- vim.api.nvim_create_autocmd('ColorScheme', {
+--     pattern = '*',
+--     callback = function()
+--         vim.api.nvim_set_hl(0, 'EyelinerPrimary', opts.primary)
+--         vim.api.nvim_set_hl(0, 'EyelinerSecondary', {})
+--     end,
+-- })
