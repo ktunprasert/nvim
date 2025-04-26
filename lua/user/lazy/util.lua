4534e9b6653a5382aefefd8e1ec09c412c22a1ae
ktunprasert <ktunprasert@outlook.com>
2025-04-26 22:33:35 +0100

    config(snacks): scratch files

--- lua/user/lazy/util.lua
+++ lua/user/lazy/util.lua
@@ -116,9 +116,19 @@ return {
         "folke/snacks.nvim",
         priority = 1000,
         lazy = false,
+        keys = {
+            ---@diagnostic disable-next-line: undefined-global
+            { "<leader>.", function() Snacks.scratch() end,        desc = "Toggle Scratch Buffer" },
+            { "<leader>S", function() Snacks.scratch.select() end, desc = "Select Scratch Buffer" },
+        },
         opts = {
             bigfile = { enabled = true },
             quickfile = { enabled = true },
+            scratch = {
+                enabled = true,
+                noautocmd = true,
+                minimal = true,
+            },
         },
     },
     {
