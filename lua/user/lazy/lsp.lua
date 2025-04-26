ecc979b71866d7e6a9355223f0b7d3f45a9d74fd
ktunprasert <ktunprasert@outlook.com>
2025-04-26 21:50:55 +0100

    perf: scissor no need to depend telescope

--- lua/user/lazy/lsp.lua
+++ lua/user/lazy/lsp.lua
@@ -276,7 +276,6 @@ return {
     {
         "chrisgrieser/nvim-scissors",
         event = "VeryLazy",
-        dependencies = { "nvim-telescope/telescope.nvim" },
         opts = {
             snippetDir = vim.fn.stdpath("config") .. "/snippets",
         },
