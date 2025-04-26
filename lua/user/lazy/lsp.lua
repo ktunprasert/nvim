ae561bf5253a1e2e7b06029a6b604e989f6e8c1a
ktunprasert <ktunprasert@outlook.com>
2025-04-26 21:50:46 +0100

    fix(avante): dont depend on shit

--- lua/user/lazy/lsp.lua
+++ lua/user/lazy/lsp.lua
@@ -62,10 +62,6 @@ return {
                 -- model = "claude-3.7-sonnet-thought",
                 model = "claude-3.7-sonnet",
             },
-            file_selector = {
-                provider = "fzf",
-                provider_opts = {},
-            },
         },
         build = "make",
         dependencies = {
@@ -73,29 +69,6 @@ return {
             "stevearc/dressing.nvim",
             "nvim-lua/plenary.nvim",
             "MunifTanjim/nui.nvim",
-            --- The below dependencies are optional,
-            "echasnovski/mini.pick",         -- for file_selector provider mini.pick
-            "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
-            -- "hrsh7th/nvim-cmp",              -- autocompletion for avante commands and mentions
-            -- "ibhagwan/fzf-lua",              -- for file_selector provider fzf
-            -- "nvim-tree/nvim-web-devicons",   -- or echasnovski/mini.icons
-            -- {
-            --     -- support for image pasting
-            --     "HakonHarnes/img-clip.nvim",
-            --     event = "VeryLazy",
-            --     opts = {
-            --         -- recommended settings
-            --         default = {
-            --             embed_image_as_base64 = false,
-            --             prompt_for_file_name = false,
-            --             drag_and_drop = {
-            --                 insert_mode = true,
-            --             },
-            --             -- required for Windows users
-            --             use_absolute_path = true,
-            --         },
-            --     },
-            -- },
             {
                 -- Make sure to set this up properly if you have lazy=true
                 'MeanderingProgrammer/render-markdown.nvim',
