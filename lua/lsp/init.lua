e27a5348d19b970e9736ff5a4334531dfb848ae4
ktunprasert <ktunprasert@outlook.com>
2025-04-27 16:53:50 +0100

    fix(lsp.init): prefer vim.iter

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -1,9 +1,5 @@
 require("lsp.setup")
 
-for _, value in pairs({
-    "lsp.null-ls",
-    "lsp.diagnostics",
-    "lsp.format",
-}) do
+vim.iter({ "lsp.null-ls", "lsp.diagnostics", "lsp.format" }):each(function(value)
     vim.schedule(function() require(value) end)
-end
+end)
