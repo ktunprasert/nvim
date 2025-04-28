57810552c4ccfc5d6f2399f0f93eb6195bb86090
ktunprasert <ktunprasert@outlook.com>
2025-04-28 13:48:32 +0100

    feat(snacks): configure zen mode window width

--- lua/user/lazy/util.lua
+++ lua/user/lazy/util.lua
@@ -120,10 +120,10 @@ return {
         lazy = false,
         keys = {
             ---@diagnostic disable-next-line: undefined-global
-            { "<leader>.", function() Snacks.scratch() end,            desc = "Toggle Scratch Buffer" },
-            { "<leader>S", function() Snacks.scratch.select() end,     desc = "Select Scratch Buffer" },
-            { "<A-f>",     function() Snacks.zen() end,                desc = "Zen Mode" },
-            { "1<A-f>",    function() Snacks.zen({ width = 100 }) end, desc = "Zen Mode (less width)" },
+            { "<leader>.", function() Snacks.scratch() end,                      desc = "Toggle Scratch Buffer" },
+            { "<leader>S", function() Snacks.scratch.select() end,               desc = "Select Scratch Buffer" },
+            { "<A-f>",     function() Snacks.zen({ win = { width = 0.8 } }) end, desc = "Zen Mode" },
+            { "1<A-f>",    function() Snacks.zen({ win = { width = 100 } }) end, desc = "Zen Mode (less width)" },
         },
         opts = {
             bigfile = { enabled = true },
