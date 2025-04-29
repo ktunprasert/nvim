462ae8679abdf29b1405ea7add90169d86b8d59e
ktunprasert <ktunprasert@outlook.com>
2025-04-29 15:16:48 +0100

    fix(avante): i dont care about cool markdown

--- lua/user/lazy/lsp.lua
+++ lua/user/lazy/lsp.lua
@@ -72,6 +72,7 @@ return {
             {
                 -- Make sure to set this up properly if you have lazy=true
                 'MeanderingProgrammer/render-markdown.nvim',
+                enabled = false,
                 opts = {
                     file_types = { "markdown", "Avante" },
                     debounce = 2000,
