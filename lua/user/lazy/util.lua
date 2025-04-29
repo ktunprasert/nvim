4ba4823d1ccb102411ec873e2351034ef4485735
ktunprasert <ktunprasert@outlook.com>
2025-04-29 10:49:28 +0100

    perf: gitsign only when in git dir

--- lua/user/lazy/util.lua
+++ lua/user/lazy/util.lua
@@ -12,9 +12,32 @@ return {
     },
     {
         "lewis6991/gitsigns.nvim",
-        event = "VeryLazy",
+        lazy = true,
+        -- event = "VeryLazy",
+        gt = { "gitcommit", "diff" },
         dependencies = "nvim-lua/plenary.nvim",
-        config = function() require("user.plugins.gitsigns") end
+        config = function() require("user.plugins.gitsigns") end,
+        init = function()
+            -- load gitsigns only when a git file is opened
+            vim.api.nvim_create_autocmd({ "BufRead" }, {
+                group = vim.api.nvim_create_augroup("GitSignsLazyLoad", { clear = true }),
+                callback = function()
+                    ---@diagnostic disable-next-line: undefined-field
+                    vim.fn.jobstart({ "git", "-C", vim.loop.cwd(), "rev-parse" },
+                        {
+                            on_exit = function(_, return_code)
+                                if return_code == 0 then
+                                    vim.api.nvim_del_augroup_by_name "GitSignsLazyLoad"
+                                    vim.schedule(function()
+                                        require("lazy").load { plugins = { "gitsigns.nvim" } }
+                                    end)
+                                end
+                            end
+                        }
+                    )
+                end,
+            })
+        end,
     },
     {
         "numToStr/Comment.nvim",
