032128411aebb7be8a3a3e88ea5b563d2256a327
ktunprasert <ktunprasert@outlook.com>
2025-04-30 22:25:54 +0100

    fix: none-ls in lazy

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -1,5 +1,5 @@
 require("lsp.setup")
 
-vim.iter({ "lsp.null-ls", "lsp.diagnostics", "lsp.format" }):each(function(value)
+vim.iter({ "lsp.diagnostics", "lsp.format" }):each(function(value)
     vim.schedule(function() require(value) end)
 end)
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -156,15 +156,57 @@ return {
     },
 
     -- LSP
-    { "neovim/nvim-lspconfig",  lazy = true, },
+    { "neovim/nvim-lspconfig" },
     {
         "williamboman/mason.nvim",
-        config = true,
-        event = "VeryLazy",
+        cmd = { "Mason", "MasonUpdate" },
+        opts = {},
     },
 
     -- Linter/Formatter
-    { "nvimtools/none-ls.nvim", event = "VeryLazy", },
+    {
+        "nvimtools/none-ls.nvim",
+        event = "LspAttach",
+        config = function()
+            local null_ls_status_ok, null_ls = pcall(require, "null-ls")
+            if not null_ls_status_ok then
+                return
+            end
+
+            -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
+            local formatting = null_ls.builtins.formatting
+            -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
+            local diagnostics = null_ls.builtins.diagnostics
+
+            local h = require("null-ls.helpers")
+            local FMT = require("null-ls.methods").internal.FORMATTING
+
+            null_ls.setup({
+                debug = true,
+                sources = {
+                    formatting.prettier.with({ extra_args = {}, filetypes = { "markdown", "md" } }),
+                    formatting.black.with({ extra_args = { "--fast" } }),
+                    formatting.biome,
+                    diagnostics.golangci_lint,
+                    -- null_ls.builtins.code_actions.gitsigns,
+                    null_ls.builtins.code_actions.ts_node_action,
+                    -- h.make_builtin(
+                    --     {
+                    --         name = "v fmt",
+                    --         method = FMT,
+                    --         filetypes = { "v" },
+                    --         generator_opts = {
+                    --             command = "v",
+                    --             args = { "fmt" },
+                    --             to_stdin = true,
+                    --         },
+                    --         factory = h.formatter_factory,
+                    --     }
+                    -- ),
+                },
+            })
+        end,
+    },
 
     -- Treesitter
     {
