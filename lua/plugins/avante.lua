e0187feba7de10d365b6f91cabebdbd56f10a684
ktunprasert <ktunprasert@outlook.com>
2025-04-30 23:34:49 +0100

    perf: avante - i fucking did it
    https://github.com/yetone/avante.nvim/discussions/1955

---
+++ lua/plugins/avante.lua
@@ -0,0 +1,81 @@
+return {
+    "yetone/avante.nvim",
+    cmd = { "AvanteToggle" },
+    keys = {
+        { "<leader>at", "<Cmd>AvanteToggle<CR>", desc = "avante: toggle" },
+    },
+    version = false, -- Never set this value to "*"! Never!
+    opts = {
+        provider = "copilot",
+        behaviour = {
+            auto_set_keymaps = true,
+        },
+        copilot = {
+            endpoint = "https://api.githubcopilot.com",
+            -- endpoint = "https://api.individual.githubcopilot.com",
+            -- model = "gpt-4o-2024-08-06",
+            -- available models
+            -- "gpt-3.5-turbo" OK
+            -- "gpt-3.5-turbo-0613" OK
+            -- "gpt-4o-mini" OK
+            -- "gpt-4o-mini-2024-07-18" OK
+            -- "gpt-4" OK
+            -- "gpt-4-0613" OK
+            -- "gpt-4o" OK
+            -- "gpt-4o-2024-05-13" OK
+            -- "gpt-4-o-preview" OK
+            -- "gpt-4o-2024-08-06" OK
+            -- "gpt-4o-2024-11-20" OK
+            -- "claude-3.5-sonnet" OK
+            -- "claude-3.7-sonnet" OK
+            -- "claude-3.7-sonnet-thought" OK
+            -- "gemini-2.0-flash-001" OK
+            --
+            -- Untested
+            -- "text-embedding-ada-002"
+            -- "text-embedding-3-small"
+            -- "text-embedding-3-small-inference"
+            --
+            -- Not OK
+            -- The following are found in the API for the Immersive chat mode in GitHub copilot
+            -- "o1" NOT OK
+            -- "o1-2024-12-17" NOT OK
+            -- "o3-mini" NOT OK
+            -- "o3-mini-2025-01-31" NOT OK
+            -- "o3-mini-paygo" NOT OK
+            -- model = "claude-3.7-sonnet-thought",
+            model = "claude-3.7-sonnet",
+        },
+        windows = {
+            wrap = true, -- similar to vim.o.wrap
+            width = 30,  -- default % based on available width
+            edit = { start_insert = false, },
+            ask = {
+                floating = false,     -- Open the 'AvanteAsk' prompt in a floating window
+                start_insert = false, -- Start insert mode when opening the ask window
+            },
+        },
+
+    },
+    build = "make",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "MunifTanjim/nui.nvim",
+        {
+            -- Make sure to set this up properly if you have lazy=true
+            'MeanderingProgrammer/render-markdown.nvim',
+            opts = {
+                file_types = { "markdown", "Avante" },
+                debounce = 2000,
+            },
+            ft = { "markdown", "Avante" },
+            overrides = {
+                buftype = {
+                    nofile = {
+                        code = { left_pad = 0, right_pad = 0 },
+                    },
+                },
+            },
+        },
+    },
+}
