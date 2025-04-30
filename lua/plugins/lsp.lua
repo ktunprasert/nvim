159685eb3308232880345d057da1346e46a8d754
ktunprasert <ktunprasert@outlook.com>
2025-05-01 00:31:35 +0100

    perf: mini.ai

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -329,7 +329,6 @@ return {
 
     {
         "echasnovski/mini.ai",
-        event = "VeryLazy",
         keys = {
             { "a", mode = { "x", "o" } },
             { "i", mode = { "x", "o" } },
