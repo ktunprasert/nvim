4694251249ad498dbfffad38388f6b0870d38920
ktunprasert <ktunprasert@outlook.com>
2025-05-02 20:04:15 +0100

    feat(lsp): enable dynamic file watching for improved workspace updates

--- lua/lsp/setup.lua
+++ lua/lsp/setup.lua
@@ -1,15 +1,14 @@
-local cap_overrides = {
-    workspace = {
-        didChangeWatchedFiles = {
-            dynamicRegistration = true,
-            relativePatternSupport = true,
-        },
-    }
-}
-
 vim.lsp.config("*", {
     flags = {
         debounce_text_changes = 500,
+    },
+    capabilities = {
+        workspace = {
+            didChangeWatchedFiles = {
+                dynamicRegistration = true,
+                relativePatternSupport = true,
+            },
+        }
     },
     -- root_dir = require("lspconfig").util.root_pattern('.git'),
     -- root_markers = require("lspconfig").util.root_pattern('.git'),
