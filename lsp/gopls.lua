cdd0c7f98bda173db24c22c3f4c873a6acc8ec03
ktunprasert <ktunprasert@outlook.com>
2025-05-02 20:04:05 +0100

    refactor(lsp): enhance gopls configuration with detailed hint settings

--- lsp/gopls.lua
+++ lsp/gopls.lua
@@ -1,7 +1,20 @@
 return {
+    capabilities = {
+        workspace = {
+            didCreateFiles = true,
+        },
+    },
     settings = {
         gopls = {
-            buildFlags = { "-tags=mine" }
-        }
+            buildFlags = { "-tags=mine" },
+            hints = {
+                assignVariableTypes = true,
+                compositeLiteralFields = true,
+                compositeLiteralTypes = true,
+                functionTypeParameters = true,
+                parameterNames = true,
+                rangeVariableTypes = true,
+            },
+        },
     }
 }
