6837ae82136beaed350d01f4cb692fa54f5d990e
ktunprasert <ktunprasert@outlook.com>
2025-05-02 12:03:15 +0100

    fix(lua): source command should apply to the current buffer only

--- after/ftplugin/lua.lua
+++ after/ftplugin/lua.lua
@@ -3,4 +3,4 @@ local keymap = require('lib.utils').keymap
 vim.opt_local.shiftwidth = 4
 
 -- source current file
-keymap("n", "<F6>", require('lib.utils').cmdcr("source"))
+keymap("n", "<F6>", require('lib.utils').cmdcr("source"), { buffer = true })
