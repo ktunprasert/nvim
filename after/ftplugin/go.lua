d014ad17374dd8728f2a9360b9188b807d9934c9
ktunprasert <ktunprasert@outlook.com>
2025-05-02 12:46:47 +0100

    feat(go): add auto-organize imports on keypress

--- after/ftplugin/go.lua
+++ after/ftplugin/go.lua
@@ -1,1 +1,7 @@
 vim.opt_local.shiftwidth = 4
+
+local function organize_imports_async()
+    vim.lsp.buf.code_action({ context = { only = { 'source.organizeImports' } }, apply = true })
+end
+
+vim.keymap.set('n', '<F6>', organize_imports_async, { desc = 'Organize Import', buffer = true })
