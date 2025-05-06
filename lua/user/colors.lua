29c59bfe01051a80cb5d85575345964283b6cb53
ktunprasert <ktunprasert@outlook.com>
2025-05-06 23:24:56 +0100

    feat(colors): enhance UI consistency by linking more elements to AshenBackground

--- lua/user/colors.lua
+++ lua/user/colors.lua
@@ -1,11 +1,14 @@
 if winblend() == 0 then
-    vim.api.nvim_set_hl(0, "WinBar", { link = "AshenBackground" })
-    vim.api.nvim_set_hl(0, "WinBarNC", { link = "AshenBackground" })
     vim.api.nvim_set_hl(0, "DapUICurrentFrameName", { link = "AshenBackground" })
     vim.api.nvim_set_hl(0, "AvanteSidebarWinHorizontalSeparator", { link = "AshenBackground" })
     vim.api.nvim_set_hl(0, "lualine_c_normal", { link = "AshenBackground" })
     vim.api.nvim_set_hl(0, "lualine_c_inactive", { link = "AshenBackground" })
     vim.api.nvim_set_hl(0, "Statusline", { link = "AshenBackground" })
+    vim.api.nvim_set_hl(0, "TabLine", { link = "AshenBackground" })
+    vim.api.nvim_set_hl(0, "TabLineFill", { link = "AshenBackground" })
+    vim.api.nvim_set_hl(0, "WinBar", { link = "AshenBackground" })
+    vim.api.nvim_set_hl(0, "WinBarNC", { link = "AshenBackground" })
+    vim.api.nvim_set_hl(0, "WinSeparator", { link = "AshenBackground" })
 end
 
 vim.api.nvim_set_hl(0, "LineNr", { link = "AshenG6" })
