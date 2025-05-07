172aeac38c1a8ea45e226c4f4370d02d7b9a0d18
ktunprasert <ktunprasert@outlook.com>
2025-05-07 21:43:51 +0100

    feat(telescope): configure frecency decay for contextual file prioritization

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -153,6 +153,14 @@ return {
                     },
                     frecency = {
                         path_display = { "filename_first" },
+                        recency_values = {
+                            { age = 240,    value = 100 }, -- past 4 hours
+                            { age = 1440,   value = 80 },  -- past day
+                            { age = 4320,   value = 60 },  -- past 3 days
+                            { age = 10080,  value = 40 },  -- past week
+                            { age = 43200,  value = 20 },  -- past month
+                            { age = 129600, value = 10 },  -- past 90 days
+                        },
                     },
                 },
             })
