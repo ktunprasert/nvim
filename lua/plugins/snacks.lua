1d85a946caf6136e5ea697596ef4e4e77c3e0b7d
ktunprasert <ktunprasert@outlook.com>
2025-05-07 23:18:21 +0100

    feat(snacks): integrate flash.nvim for enhanced picker navigation

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -9,6 +9,9 @@ local cwd_opts = {
 }
 return {
     "folke/snacks.nvim",
+    dependencies = {
+        "folke/flash.nvim",
+    },
     priority = 1000,
     lazy = false,
     keys = {
@@ -94,6 +97,8 @@ return {
             win = {
                 input = {
                     keys = {
+                        ["<a-s>"] = { "flash", mode = { "n", "i" } },
+                        ["s"] = { "flash" },
                         ["<M-j>"] = { "edit_split", mode = { "i", "n" } },
                         [""] = { "toggle_help_list", mode = { "i" } },
                         ["<M-l>"] = { "edit_vsplit", mode = { "i", "n" } },
@@ -111,7 +116,27 @@ return {
                     builtin = false,
                     cmd = { "delta", "--side-by-side" },
                 },
-            }
+            },
+            actions = {
+                flash = function(picker)
+                    require("flash").jump({
+                        pattern = "^",
+                        label = { after = { 0, 0 }, style = "overlay" },
+                        search = {
+                            mode = "search",
+                            exclude = {
+                                function(win)
+                                    return vim.bo[vim.api.nvim_win_get_buf(win)].filetype ~= "snacks_picker_list"
+                                end,
+                            },
+                        },
+                        action = function(match)
+                            local idx = picker.list:row2idx(match.pos[1])
+                            picker.list:_move(idx, true, true)
+                        end,
+                    })
+                end,
+            },
         }
         ,
         bigfile = { enabled = true },
