ba13aa6fe4b77ce0856f246bbe7dbd3235cca0e6
ktunprasert <ktunprasert@outlook.com>
2025-05-08 23:56:34 +0100

    feat(codecompanion): add history picker customization

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -28,6 +28,9 @@ return {
             extensions = {
                 history = {
                     enabled = true,
+                    opts = {
+                        picker = "snacks",
+                    },
                 },
             },
         },
