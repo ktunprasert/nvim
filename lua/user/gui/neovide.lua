45f767d67a96e59316d9c3cf12f4c05678eb4a32
ktunprasert <ktunprasert@outlook.com>
2025-05-11 02:58:44 +0100

    feat(neovide): add zoom in/out keybinds

--- lua/user/gui/neovide.lua
+++ lua/user/gui/neovide.lua
@@ -4,3 +4,7 @@ vim.keymap.set('n', '<C-v>', '"+P')         -- Paste normal mode
 vim.keymap.set('v', '<C-v>', '"+P')         -- Paste visual mode
 vim.keymap.set('c', '<C-v>', '<C-R>+')      -- Paste command mode
 vim.keymap.set('i', '<C-v>', '<ESC>l"+Pli') -- Paste insert mode
+
+vim.keymap.set({ "n", "v" }, "<C-+>", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor + 0.1<CR>")
+vim.keymap.set({ "n", "v" }, "<C-=>", ":lua vim.g.neovide_scale_factor = vim.g.neovide_scale_factor - 0.1<CR>")
+vim.keymap.set({ "n", "v" }, "<C-0>", ":lua vim.g.neovide_scale_factor = 1<CR>")
