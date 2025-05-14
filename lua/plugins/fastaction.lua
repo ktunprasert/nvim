15701c78ced0fee7a4a6ef8ced025fdaa00c5357
ktunprasert <ktunprasert@outlook.com>
2025-05-14 01:15:02 +0100

    feat: add flash actions and multicursor integration

--- lua/plugins/fastaction.lua
+++ lua/plugins/fastaction.lua
@@ -3,38 +3,62 @@ local flash_util = require("lib.flash_util")
 local menus = {
     items = {
         " hop",
+        " jump",
+        " remote",
         " treesitter",
         " treesitter search",
         " continue",
+        "󰇀 all <cword>",
+        "󰇀 all search term",
     },
     fns = {
         function() flash_util.flash_word() end,
+        function() require("flash").jump() end,
+        function() require("flash").remote() end,
         function() require("flash").treesitter({}) end,
         function() require("flash").treesitter_search() end,
         function() require("flash").jump({ continue = true }) end,
+        function() require("multicursor-nvim").matchAllAddCursors() end,
+        function() require("multicursor-nvim").searchAllAddCursors() end,
+    },
+    keys = {
+        [" hop"] = "<C-Space>",
+        [" jump"] = "j",
+        ["󰇀 all <cword>"] = "m",
+        ["󰇀 all search term"] = "s",
     },
 }
+
+local override = function(params)
+    if params.kind == "actions" then
+        if menus.keys[params.title] ~= nil then
+            return { key = menus.keys[params.title] }
+        end
+    end
+end
 
 
 return {
     {
         'Chaitanyabsprip/fastaction.nvim',
+        dependencies = { "jake-stewart/multicursor.nvim" },
         keys = {
             { "<F3>", function() require("fastaction").code_action() end, mode = { "v", "n" } },
             {
                 "<c-space>",
                 function()
-                    vim.ui.select(menus.items, { prompt = 'Actions', },
-                        function(choice, idx)
-                            menus.fns[idx]()
-                        end)
+                    vim.ui.select(
+                        menus.items,
+                        { prompt = 'Actions', kind = "actions", },
+                        function(_, idx) menus.fns[idx]() end
+                    )
                 end,
             },
         },
         opts = {
             dismiss_keys = { "j", "k", "<c-c>", "q", "<Esc>" },
             register_ui_select = true,
-            -- override_function = function()end, -- TODO: implement a "number-first" key quick jump
+            override_function = override,
             priority = {
                 go = {
                     { pattern = "organize import", key = "o", order = 1 },
@@ -47,6 +71,11 @@ return {
                     key = "AshenRedFlame",
                 },
             },
+            -- format_right_section = function(item)
+            --     if item.ctx ~= nil then
+            --         return vim.lsp.get_client_by_id(item.ctx.client_id).name
+            --     end
+            -- end,
         },
     },
 
