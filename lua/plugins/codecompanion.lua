d2c5191239a853c92c61bd07594343de768efd33
ktunprasert <ktunprasert@outlook.com>
2025-05-16 09:43:08 +0100

    fix(codecompanion): gemini back baby
    https://www.githubstatus.com/incidents/pzrqw1n5ynw3

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -18,7 +18,7 @@ return {
                     return require("codecompanion.adapters").extend("copilot", {
                         schema = {
                             model = {
-                                default = "gpt-4.1",
+                                default = "gemini-2.5-pro-preview-05-06"
                             }
                         }
                     })
