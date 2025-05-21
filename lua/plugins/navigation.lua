6e50cf35d74fd705a4371ed885e9936f4cf155c9
ktunprasert <ktunprasert@outlook.com>
2025-05-21 15:30:56 +0100

    feat(navigation): add motion-enhanced word navigation with Spider keybindings

--- lua/plugins/navigation.lua
+++ lua/plugins/navigation.lua
@@ -67,4 +67,16 @@ return {
             },
         },
     },
+    {
+        "chrisgrieser/nvim-spider",
+        lazy = true,
+        keys = {
+            { "w",     "<cmd>lua require('spider').motion('w')<CR>", mode = { "n", "x" } },
+            { "e",     "<cmd>lua require('spider').motion('e')<CR>", mode = { "n", "x" } },
+            { "b",     "<cmd>lua require('spider').motion('b')<CR>", mode = { "n", "x" } },
+            { "<C-f>", "<cmd>lua require('spider').motion('w')<CR>", mode = { "i" } },
+            { "<C-b>", "<cmd>lua require('spider').motion('b')<CR>", mode = { "i" } },
+            { "<C-e>", "<cmd>lua require('spider').motion('e')<CR>", mode = { "i" } },
+        },
+    },
 }
