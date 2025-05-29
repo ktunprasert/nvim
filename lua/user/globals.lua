2abf23f2fa14b6e4b66c8b0bbfc9c2e85ec78853
ktunprasert <ktunprasert@outlook.com>
2025-05-29 22:42:20 +0100

    fix(netrw): we need it

--- lua/user/globals.lua
+++ lua/user/globals.lua
@@ -21,5 +21,5 @@ end
 vim.keymap.set("", "<Space>", "<Nop>")
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
+-- vim.g.loaded_netrw = 1
+-- vim.g.loaded_netrwPlugin = 1
