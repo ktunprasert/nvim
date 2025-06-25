e35cba1dbd5c9a7b5291e4aef0a91330feada84d
ktunprasert <ktunprasert@outlook.com>
2025-06-25 15:59:19 +0100

    fix(codecompanion): oops model is 06-05 not versa

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -18,7 +18,7 @@ return {
                     return require("codecompanion.adapters").extend("copilot", {
                         schema = {
                             model = {
-                                default = "gemini-2.5-pro-preview-05-06"
+                                default = "gemini-2.5-pro-preview-06-05"
                                 -- default = "claude-sonnet-4"
                             }
                         }
