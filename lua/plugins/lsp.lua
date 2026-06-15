8c55475badbe61284cee26b9b4192e7e3bb75de9
ktunprasert <ktunprasert@outlook.com>
2026-06-15 12:57:43 +0100

    fix(treesitter): trigger highlighting manually

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -379,6 +379,14 @@ return {
         -- lazy = false,
         event = { "BufReadPre" },
         build = ":TSUpdate",
+        init = function()
+            vim.api.nvim_create_autocmd('FileType', {
+                callback = function()
+                    pcall(vim.treesitter.start)
+                    vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+                end,
+            })
+        end,
     },
     {
         "hedyhli/outline.nvim",
